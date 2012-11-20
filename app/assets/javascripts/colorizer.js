21b6519ab80a521e28d8e708eb2611f499da186e
DBC Apprentice <apprentice@devbootcamp.com>
2012-11-19 18:35:57 -0800

    Turn colorize into a jQuery plugin

--- app/assets/javascripts/colorizer.js
+++ app/assets/javascripts/colorizer.js
@@ -1,26 +1,26 @@
-var colorizeList = function() {
-  // Colorize the text of a list title
-  // Step 1: grab the text
-  var list = $('h1 span.best_in_place');
+(function( $ ){
+  $.fn.colorize = function() {
 
-  // Step 2: split the text into individual chars
-  var chars = list.text().split('');
+    return this.each(function() {
+      var chars = $(this).text().split('');
 
-  // Step 3: wrap each character in a span tag with the appropriate color
-  var colors = ["#097054", "#FFDE00", "#6599FF", "#FF9900", "#587058",
-                "#587498", "#E86850", "#99CCFF", "#FF9900", "#00CC00", "#993366"];
+      // Step 3: wrap each character in a span tag with the appropriate color
+      var colors = ["#097054", "#FFDE00", "#6599FF", "#FF9900", "#587058",
+                    "#587498", "#E86850", "#99CCFF", "#FF9900", "#00CC00", "#993366"];
 
-  for (var i = 0; i < chars.length; i++) {
-    var letter = chars[i];
-    chars[i] = "<span style = 'color: " + colors[i % colors.length] + "'>" + letter + "</span>"
+      for (var i = 0; i < chars.length; i++) {
+        var letter = chars[i];
+        chars[i] = "<span style = 'color: " + colors[i % colors.length] + "'>" + letter + "</span>"
+      };
+
+      // Step 4: replace the html of the list with the colorized
+      $(this).html(chars);
+    });
   };
-
-  // Step 4: replace the html of the list with the colorized
-  list.html(chars);
-};
+})(jQuery);
 
 $(document).ready(function() {
-  colorizeList();
-
-  $('h1 span.best_in_place').bind("best_in_place:deactivate", function() { colorizeList() });
+  $('h1 span.best_in_place').colorize().on("best_in_place:deactivate", function() {
+    $(this).colorize();
+  });
 });
