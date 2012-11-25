831be8d84b131311ce07467ba6d7188b3254d7d3
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-24 17:04:22 -0800

    got reminders working in-time, need to add future functionality

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -6,6 +6,22 @@ class Reminder < ActiveRecord::Base
 
   def send_sms
     @task = Reminder.last.task
-    RemindersWorker.perform_async(@task.id)
+    current_user = Reminder.last.task.list.user
+    account_sid = "ACe3f08f26920946de8394b08ad903b161"
+    auth_token = "c86b4df4b436dbf4f4b202ef11196d46"
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    from = "+18473803186" # Your Twilio number
+    contact_info = {
+    current_user.phone_number => task.description,
+    }
+    contact_info.each do |key, value|
+      client.account.sms.messages.create(
+        :from => from,
+        :to => key,
+        :body => value
+      )
+      puts "Sent message to #{value}"
+    end
   end
 end
